cfbf10e4e6b944234d1614268e7b07e637139611
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2022-06-08 10:25:27 -0400

    Update unit test to make sure regular item decreases in quality

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,9 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+describe("A regular item with quality 10", function() {
+  it("should decrease in quality to 9 after a day", function() {
+    const gildedRose = new Shop([new Item("sword", 10, 10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].quality).toBe(9);
   });
 });
