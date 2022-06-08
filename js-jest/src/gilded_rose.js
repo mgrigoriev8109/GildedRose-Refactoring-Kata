fb30508cc841758899bfe7a868b9273583fafdd5
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2022-06-08 13:42:24 -0400

    Finish extracting methods

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,56 +15,77 @@ class Shop {
 
     for (let i = 0; i < this.items.length; i++) {
       let item = this.items[i]
-      if (this.regularItem(item)) {
+      if (this.regularItem(item) && this.hasQuality(item)) {
         this.decreaseQuality(item)
       }
         
       else {
-        this.increaseQuality(item)
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sellIn < 11) {
-            this.increaseQuality(item)
-          }
-          
-          if (item.sellIn < 6) {
-            this.increaseQuality(item)
-          }
-          
-        }
-        
-      }
-      if (this.nonLegendaryItem(item)) {
-        item.sellIn = item.sellIn - 1;
+        this.betterWithAge(item)
       }
 
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (this.nonLegendaryItem(item)) {
-                this.decreaseQuality(item)
-              }
-            }
-          } else {
-            item.quality = 0
-          }
-        } else {
-          this.increaseQuality(item)
-        }
-      }
+      this.decreaseSellIn(item)
+
+      this.pastSellInDate(item)
     }
 
     return this.items;
   }
 
+
+  pastSellInDate(item){
+    if (item.sellIn < 0) {
+      if (item.name != 'Aged Brie') {
+        if (this.regularItem) {
+              this.decreaseQuality(item)
+        } else {
+          item.quality = 0
+        }
+      } else {
+        this.increaseQuality(item)
+      }
+    }
+  }
+  
+  hasQuality(item){
+    if (item.quality > 0){
+      return true
+    }
+  }
+  betterWithAge(item){ 
+    let betterWithAgeItems = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert']
+    if (betterWithAgeItems.includes(item.name)){
+      this.increaseQuality(item)
+      this.increaseConcertQuality(item)
+    }
+  }
+
+  increaseConcertQuality(item){
+    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.sellIn < 11) {
+        this.increaseQuality(item)
+      }
+      
+      if (item.sellIn < 6) {
+        this.increaseQuality(item)
+      }
+      
+    }
+  }
+
+  decreaseSellIn(item){
+    if (this.nonLegendaryItem(item)) {
+      item.sellIn = item.sellIn - 1;
+    }
+  }
   nonLegendaryItem(item){
     item.name != 'Sulfuras, Hand of Ragnaros'
   }
 
+
   regularItem(item){
     let nonRegularNames = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros']
     
-    if(item.quality > 0 && !nonRegularNames.includes(item.name)){
+    if(!nonRegularNames.includes(item.name)){
       return true
     }
   }
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -8,6 +8,22 @@ describe("A regular item with quality 10", function() {
   });
 });
 
+describe("Once the sell by date has passed", function() {
+  it("Quality degrades twice as fast", function() {
+    const gildedRose = new Shop([new Item("sword", -1, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(48);
+  });
+});
+
+describe("The Quality of an item", function() {
+  it("is never negative", function() {
+    const gildedRose = new Shop([new Item("sword", 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+});
+
 describe("An aged brie with quality 50", function() {
   it("Should not become 51, but stay at quality 50", function() {
     const gildedRose = new Shop([new Item("Aged Brie", 50, 50)]);
@@ -23,3 +39,4 @@ describe("An legendary item with quality 80", function() {
     expect(items[0].quality).toBe(80);
   });
 });
+
